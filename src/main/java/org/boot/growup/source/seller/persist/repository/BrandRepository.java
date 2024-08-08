3bb49820d7a2ce263e57562bf9eb890db4173c48
정태승 <hi563@g.skku.edu>
2024-08-08 21:10:52 +0900

    fix: Brand, Seller Repository 푸시

---
+++ src/main/java/org/boot/growup/source/seller/persist/repository/BrandRepository.java
@@ -0,0 +1,10 @@
+package org.boot.growup.source.seller.persist.repository;
+
+import org.boot.growup.source.seller.persist.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+    Optional<Brand> findByName(String name);
+}
