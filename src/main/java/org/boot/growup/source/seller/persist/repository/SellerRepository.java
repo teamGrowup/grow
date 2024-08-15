df46de86eb1be123860a06085745ced635e13877
WoongHee Jo <xcv415@naver.com>
2024-08-15 17:46:31 +0900

    refactor: SellerRepository confilct

--- src/main/java/org/boot/growup/source/seller/persist/repository/SellerRepository.java
+++ src/main/java/org/boot/growup/source/seller/persist/repository/SellerRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SellerRepository extends JpaRepository<Seller, Long> {
-    Optional<Seller> findById(Long id);
 }
