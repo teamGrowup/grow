be355de1e2d7248e0fbb9a4a581a23038de72d9f
WoongHee Jo <xcv415@naver.com>
2024-08-15 17:44:15 +0900

    refactor: Service 주석 추가

--- src/main/java/org/boot/growup/source/seller/service/ProductImageService.java
+++ src/main/java/org/boot/growup/source/seller/service/ProductImageService.java
@@ -2,13 +2,17 @@ package org.boot.growup.source.seller.service;
 
 import org.boot.growup.common.enumerate.Section;
 import org.boot.growup.source.seller.persist.entity.Product;
-import org.boot.growup.source.seller.persist.entity.ProductImage;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ProductImageService {
-
+    /*
+    상품 이미지 등록
+     */
     void saveProductImages(List<MultipartFile> productImageFiles, Product product, Section section);
+    /*
+    등록된 상품 이미지 수정
+     */
     void updateProductImages(List<MultipartFile> productImages, Product product, Section section);
 }
--- src/main/java/org/boot/growup/source/seller/service/ProductService.java
+++ src/main/java/org/boot/growup/source/seller/service/ProductService.java
@@ -10,8 +10,20 @@ import java.util.List;
 
 @Service
 public interface ProductService {
+    /*
+    상품 등록
+     */
     Product registerProduct(ProductRequestDTO productRequestDto, Seller seller);
+    /*
+    등록된 상품 수정
+     */
     Product updateProduct(ProductRequestDTO productRequestDto, Seller seller, Long productId);
+    /*
+    등록된 상품의 권한 변경 > 허가, 대기, 거부
+     */
     void changeProductAuthority(Long productId, AuthorityStatus status);
+    /*
+    권한 상태에 따른 상품 검색
+     */
     List<Product> readProductRequestsByStatus(AuthorityStatus authorityStatus, int pageNo);
 }
