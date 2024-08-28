588741d3ade7f86e584c08a451c425c36557a5bd
WoongHee Jo <xcv415@naver.com>
2024-08-28 19:59:02 +0900

    refactor: @header 수정

--- src/main/java/org/boot/growup/review/controller/ReviewController.java
+++ src/main/java/org/boot/growup/review/controller/ReviewController.java
@@ -24,7 +24,7 @@ public class ReviewController {
     /**
      * [POST]
      * 리뷰 등록 요청
-     * @header null
+     * @header Customer's AccessToken
      * @body PostReviewRequestDTO, MultipartFile[]
      * @response Review
      */
@@ -40,7 +40,7 @@ public class ReviewController {
     /**
      * [GET]
      * 특정 리뷰 조회
-     * @header null
+     * @header Customer's AccessToken
      * @param reviewId 리뷰 ID
      * @response Optional<Review>
      */
@@ -53,6 +53,7 @@ public class ReviewController {
     /**
      * [GET]
      * 모든 리뷰 조회
+     * @header Admin's AccessToken
      * @response BaseResponse<List<ReviewResponseDTO>>
      */
     @GetMapping("/admins/reviews")
@@ -64,6 +65,7 @@ public class ReviewController {
     /**
      * [GET]
      * 특정 브랜드의 모든 리뷰 조회
+     * @header Customer's AccessToken
      * @param brandId 브랜드 ID
      * @response BaseResponse<BrandReviewsResponseDTO>
      */
@@ -76,7 +78,7 @@ public class ReviewController {
     /**
      * [PATCH]
      * 구매자의 리뷰 수정
-     * @header null
+     * @header Customer's AccessToken
      * @param reviewId 리뷰 ID
      * @body PostReviewRequestDTO
      * @response String
@@ -94,7 +96,7 @@ public class ReviewController {
     /**
      * [DELETE]
      * 구매자의 리뷰 삭제
-     * @header null
+     * @header Customer's AccessToken
      * @param reviewId 리뷰 ID
      * @response String
      */
@@ -107,7 +109,7 @@ public class ReviewController {
     /**
      * [DELETE]
      * 관리자의 리뷰 삭제
-     * @header null
+     * @header Admin's AccessToken
      * @param reviewId 리뷰 ID
      * @response String
      */
@@ -120,11 +122,11 @@ public class ReviewController {
     /**
      * [POST]
      * 리뷰에 좋아요 추가 요청
-     * @header null
+     * @header Customer's AccessToken
      * @param reviewId 리뷰 ID
      * @response String
      */
-    @PostMapping("/{reviewId}/like")
+    @PostMapping("/customers/reviews/{reviewId}/like")
     public BaseResponse<String> postReviewLike(@PathVariable Long reviewId) {
         reviewApplication.postReviewLike(reviewId);
         return new BaseResponse<>("리뷰에 좋아요가 추가되었습니다.");
@@ -133,11 +135,11 @@ public class ReviewController {
     /**
      * [DELETE]
      * 리뷰 좋아요 삭제 요청
-     * @header null
+     * @header Customer's AccessToken
      * @param reviewId 리뷰 ID
      * @response String
      */
-    @DeleteMapping("/{reviewId}/like")
+    @DeleteMapping("/customers/reviews/{reviewId}/like")
     public BaseResponse<String> deleteReviewLike(@PathVariable Long reviewId) {
         reviewApplication.deleteReviewLike(reviewId);
         return new BaseResponse<>("리뷰 좋아요가 삭제되었습니다.");
